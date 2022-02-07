59374501e0f0d88ec233a7029a1f78b4cf0e019b
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-02-07 16:57:07 +0200

    initial commit

---
+++ src/context.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
+
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
+// import { useGlobalContext } from './context'
+// const { openSidebar, openModal } = useGlobalContext();
